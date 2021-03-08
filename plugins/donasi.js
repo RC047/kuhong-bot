52b4527aa97622200cfa201439593ee59a8320da
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-08 13:15:39 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
 ╭─「 DONATION 」
-│ • SAWERIA :
-│ https://saweria.co/donate/RC047
-│ • OVO [+62895337278647]
-│ • TRI [+62895337278647]
-│ • DANA [+62895337278647]
-│ • GOPAY [+62895337278647]
+│• SAWERIA :
+│https://saweria.co/donate/RC047
+│• OVO [+62895337278647]
+│• TRI [+62895337278647]
+│• DANA [+62895337278647]
+│• GOPAY [+62895337278647]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
