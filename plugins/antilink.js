5f4a440f8023bd3c468c1bedc87826f0e8216933
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-24 14:26:25 +0700

    Fitur AntiLink skrng work
    Kebanned tanggung sendiri :v

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -1,17 +1,22 @@
-let handler = m => m
+let handler = async function(m, { conn , args, isAdmin, isBotAdmin }) {
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = m => {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let username = conn.getName(who)
   let mentionedJid = [m.sender]
   let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+  let users = m.sender
 
-  if (chat.antiLink && isGroupLink) conn.reply(m.chat, `*「 ANTI LINK 」*\n\nTerdeteksi ${name.vnmae || name.notify || namename || ('+' + name.jid.split`@`[0])} telah mengirim link group!`)
-
-  return true
+    conn.reply(m.chat, `*「 ANTI LINK 」*\n\nTerdeteksi *${username}* telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini!`, m)
+ await conn.groupRemove(m.chat, [users])
 }
+handler.tags = ['group']
+handler.customPrefix = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.command = new RegExp
 handler.group = true
+
+handler.admin = false
+handler.botAdmin = true
+
+handler.fail = null
 
 module.exports = handler
