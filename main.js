1799d14cdcdc26abcb74ee0608df7afdad6a7c03
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-29 08:13:03 +0700

    Fix Erorr No Respon

--- main.js
+++ main.js
@@ -1,60 +1,43 @@
-let { WAConnection: _WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
+require('./config.js')
+let { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
 let { generate } = require('qrcode-terminal')
-let qrcode = require('qrcode')
-let simple = require('./lib/simple')
-let logs = require('./lib/logs')
-let yargs = require('yargs/yargs')
 let syntaxerror = require('syntax-error')
-let fetch = require('node-fetch')
-let chalk = require('chalk')
-let fs = require('fs')
-let path = require('path')
-let util = require('util')
-let { spawnSync } = require('child_process')
+let simple = require('./lib/simple')
+//  let logs = require('./lib/logs')
+let { promisify } = require('util')
+let yargs = require('yargs/yargs')
 let Readline = require('readline')
+let cp = require('child_process')
+let qrcode = require('qrcode')
+let path = require('path')
+let fs = require('fs')
+
 let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
-// RECODE LAGI IZIN LAGI!!!
 
-global.owner = ['62895337278647'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
-global.APIs = { // API Prefix
-  // name: 'https://website'
-  nrtm: 'https://nurutomo.herokuapp.com',
-  xteam: 'https://api.xteam.xyz'
-}
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'test'
-}
-
-
-global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '')
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 global.timestamp = {
   start: new Date
 }
 // global.LOGGER = logs()
 const PORT = process.env.PORT || 3000
-let opts = yargs(process.argv.slice(2)).exitProcess(false).parse()
-global.opts = Object.freeze({...opts})
-global.prefix = new RegExp('^[' + (opts['prefix'] || '‎ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\\/i!#$%*,()@1234567890¡¿\\-+£¢€¥^°=¶∆×÷π√✓©®:;?&.') + ']')
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+
+global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
 global.DATABASE = new (require('./lib/database'))(`${opts._[0] ? opts._[0] + '_' : ''}database.json`, null, 2)
 if (!global.DATABASE.data.users) global.DATABASE.data = {
   users: {},
-  groups: {},
   chats: {},
+  stats: {},
 }
-if (!global.DATABASE.data.groups) global.DATABASE.data.groups = {}
 if (!global.DATABASE.data.chats) global.DATABASE.data.chats = {}
 if (!global.DATABASE.data.stats) global.DATABASE.data.stats = {}
 if (opts['server']) {
   let express = require('express')
   global.app = express()
   app.all('*', async (req, res) => {
-    await global.conn.connect().catch(console.log)
     res.end(await qrcode.toBuffer(global.qr))
   })
   app.listen(PORT, () => console.log('App listened on port', PORT))
@@ -62,9 +45,10 @@ if (opts['server']) {
 global.conn = new WAConnection()
 let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
+if (opts['trace']) conn.logger.level = 'trace'
+if (opts['debug']) conn.logger.level = 'debug'
 if (opts['big-qr'] || opts['server']) conn.on('qr', qr => generate(qr, { small: false }))
 if (opts['server']) conn.on('qr', qr => { global.qr = qr })
-conn.on('credentials-updated', () => fs.writeFileSync(authFile, JSON.stringify(conn.base64EncodedAuthInfo())))
 let lastJSON = JSON.stringify(global.DATABASE.data)
 if (!opts['test']) setInterval(() => {
   conn.logger.info('Saving database . . .')
@@ -76,297 +60,8 @@ if (!opts['test']) setInterval(() => {
   }
 }, 60 * 1000) // Save every minute
 
-const isNumber = x => typeof x === 'number' && !isNaN(x)
-conn.handler = async function (m) {
-  try {
-  	simple.smsg(this, m)
-    m.exp = 0
-    m.limit = false
-    try {
-      let user
-      if (user = global.DATABASE._data.users[m.sender]) {
-        if (!isNumber(user.exp)) user.exp = 0
-        if (!isNumber(user.limit)) user.limit = 10
-        if (!isNumber(user.lastclaim)) user.lastclaim = 0
-        if (!isNumber(user.afk)) user.afk = -1
-        if (!'afkReason' in user) user.afkReason = ''
-      } else global.DATABASE._data.users[m.sender] = {
-        exp: 0,
-        limit: 10,
-        lastclaim: 0,
-      }
-      
-      let chat
-      if (chat =  global.DATABASE._data.chats[m.chat]) {
-        if (!'isBanned' in chat) chat.isBanned = false
-        if (!'welcome' in chat) chat.welcome = true
-        if (!'sWelcome' in chat) chat.sWelcome = ''
-        if (!'sBye' in chat) chat.sBye = ''
-        if (!'delete' in chat) chat.delete = true
-      } else global.DATABASE._data.chats[m.chat] = {
-        isBanned: false,
-        welcome: true,
-        sWelcome: '',
-        sBye: '',
-        delete: true
-      }
-    } catch (e) {
-      console.log(e, global.DATABASE.data)
-    }
-    if (!m.fromMe && opts['self']) return
-    if (!m.text) return
-    if (m.isBaileys) return
-    m.exp += 1
-    
-  	let usedPrefix
-    let _user = global.DATABASE._data.users[m.sender]
-  	for (let name in global.plugins) {
-  	  let plugin = global.plugins[name]
-      if (!plugin) continue
-      if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) continue
-      let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
-  	  if ((usedPrefix = (_prefix.exec(m.text) || '')[0])) {
-        let noPrefix = m.text.replace(usedPrefix, '')
-  		  let [command, ...args] = noPrefix.trim().split` `.filter(v=>v)
-        args = args || []
-        let _args = noPrefix.trim().split` `.slice(1)
-        let text = _args.join` `
-  		  command = (command || '').toLowerCase()
-        let isROwner = [global.conn.user.jid, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-        let isOwner = isROwner || m.fromMe
 
-  			let isAccept = plugin.command instanceof RegExp ? // RegExp Mode?
-          plugin.command.test(command) :
-          Array.isArray(plugin.command) ? // Array?
-            plugin.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
-              cmd.test(command) :
-              cmd === command
-            ) :
-            typeof plugin.command === 'string' ? // String?
-              plugin.command === command :
-              false
 
-  			if (!isAccept) continue
-        m.plugin = name
-        let isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-        let isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-        let groupMetadata = m.isGroup ? await this.groupMetadata(m.chat) : {}
-        let participants = m.isGroup ? groupMetadata.participants : []
-        let user = m.isGroup ? participants.find(u => u.jid == m.sender) : {} // User Data
-        let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {} // Your Data
-        let isAdmin = user.isAdmin || user.isSuperAdmin || false // Is User Admin?
-        let isBotAdmin = bot.isAdmin || bot.isSuperAdmin || false // Are you Admin?
-        if (m.chat in global.DATABASE._data.chats) {
-          let chat = global.DATABASE._data.chats[m.chat]
-          if (name != 'unbanchat.js' && chat && chat.isBanned) return // Except this
-        }
-        if (plugin.before && plugin.before({
-          usedPrefix
-        })) return
-        let fail = plugin.fail || global.dfail // When failed
-        if (plugin.rowner && !isROwner) { // Real Owner
-          fail('rowner', m, this)
-          continue
-        }
-        if (plugin.owner && !isOwner) { // Number Owner
-          fail('owner', m, this)
-          continue
-        }
-        if (plugin.mods && !isMods) { // Moderator
-          fail('mods', m, this)
-          continue
-        }
-        if (plugin.premium && !isPrems) { // Premium
-          fail('premium', m, this)
-          continue
-        }
-  			if (plugin.group && !m.isGroup) { // Group Only
-          fail('group', m, this)
-          continue
-        } else if (plugin.botAdmin && !isBotAdmin) { // You Admin
-          fail('botAdmin', m, this)
-          continue
-        } else if (plugin.admin && !isAdmin) { // User Admin
-          fail('admin', m, this)
-          continue
-        }
-  			if (plugin.private && m.isGroup) { // Private Chat Only
-          fail('private', m, this)
-          continue
-        }
-        if (plugin.register && _user.registered == false) { // Butuh daftar?
-          fail('unreg', m, this)
-          continue
-        }
-
-        m.isCommand = true
-        let xp = 'exp' in plugin ? parseInt(plugin.exp) : 9 // XP Earning per command
-        if (xp > 999) m.reply('Ngecit -_-') // Hehehe
-        else m.exp += xp
-        if (!isPrems && global.DATABASE._data.users[m.sender].limit < m.limit * 1 && plugin.limit) {
-          this.reply(m.chat, `Limit anda habis, silahkan beli melalui *${usedPrefix}buy*`, m)
-          continue // Limit habis
-        }
-        try {
-          await plugin.call(this, m, {
-            usedPrefix,
-            noPrefix,
-            _args,
-            args,
-            command,
-            text,
-            conn: this,
-            participants,
-            groupMetadata,
-            isROwner,
-            isOwner,
-            isAdmin,
-            isBotAdmin,
-            isPrems
-          })
-          if (!isPrems) m.limit = m.limit || plugin.limit || false
-        } catch (e) {
-          // Error occured
-          m.error = e
-          console.log(e)
-          if (e) m.reply(util.format(e))
-        } finally {
-          // m.reply(util.format(_user)) 
-          if (m.limit) m.reply(+ m.limit + ' Limit terpakai')
-        }
-  			break
-  		}
-  	}
-  } finally {
-    //console.log(global.DATABASE._data.users[m.sender])
-    let user, stats = global.DATABASE._data.stats
-    if (m) {
-      if (m.sender && (user = global.DATABASE._data.users[m.sender])) {
-        user.exp += m.exp
-        user.limit -= m.limit * 1
-      }
-      
-      let stat
-      if (m.plugin) {
-        let now = + new Date
-        if (m.plugin in stats) {
-          stat = stats[m.plugin]
-          if (!isNumber(stat.total)) stat.total = 1
-          if (!isNumber(stat.success)) stat.success = m.error ? 0 : 1
-          if (!isNumber(stat.last)) stat.last = now
-          if (!isNumber(stat.lastSuccess)) stat.lastSuccess = m.error ? 0 : now
-        } else stat = stats[m.plugin] = {
-          total: 1,
-          success: m.error ? 0 : 1,
-          last: now,
-          lastSuccess: m.error ? 0 : now
-        }
-        stat.total += 1
-        stat.last = now
-        if (!m.error) {
-          stat.success += 1
-          stat.lastSuccess = now
-        }
-      }
-    } 
-    
-    try {
-      require('./lib/print')(m, this)
-    } catch (e) {
-      console.log(m, m.quoted, e)
-    }
-  }
-}
-conn.welcome = 'Halo @user!\n\nSelamat datang di grup\n*@subject*\n\nJangan Lupa Intronya ya!'
-conn.bye = 'Al-Fatihah buat yang Left!\n\n@user'
-conn.onAdd = async function ({ m, participants }) {
-  let chat = global.DATABASE._data.chats[m.key.remoteJid]
-  if (!chat.welcome) return
-  for (let user of participants) {
-    let pp = './src/avatar_contact.png'
-    try {
-      pp = await this.getProfilePicture(user)
-    } catch (e) {
-    } finally {
-      let text = (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@user', '@' + user.split('@')[0]).replace('@subject', this.getName(m.key.remoteJid))
-      this.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, {
-        contextInfo: {
-          mentionedJid: [user]
-        }
-      })
-    }
-  }
-}
-
-conn.onLeave = async function  ({ m, participants }) {
-  let chat = global.DATABASE._data.chats[m.key.remoteJid]
-  if (!chat.welcome) return
-  for (let user of participants) {
-    if (this.user.jid == user) continue
-    let pp = './src/avatar_contact.png'
-    try {
-      pp = await this.getProfilePicture(user)
-    } catch (e) {
-    } finally {
-      let text = (chat.sBye || this.bye || conn.bye || 'Bye, @user!').replace('@user', '@' + user.split('@')[0])
-      this.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, {
-        contextInfo: {
-          mentionedJid: [user]
-        }
-      })
-    }
-  }
-}
-
-conn.onDelete = async function (m) {
-  if (m.key.fromMe) return
-  let chat = global.DATABASE._data.chats[m.key.remoteJid]
-  if (chat.delete) return
-  await this.reply(m.key.remoteJid, `
-*「 ANTI DELETE PESAN 」*
-
-@${m.participant.split`@`[0]} telah menghapus pesan!
-`.trim(), m.message, {
-    contextInfo: {
-      mentionedJid: [m.participant]
-    }
-  })
-  this.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
-}
-
-conn.on('message-new', conn.handler)
-conn.on('message-delete', conn.onDelete)
-conn.on('group-add', conn.onAdd)
-conn.on('group-leave', conn.onLeave)
-conn.on('error', conn.logger.error)
-conn.on('close', () => {
-  setTimeout(async () => {
-    try {
-      if (conn.state === 'close') {
-        await conn.loadAuthInfo(authFile)
-        await conn.connect()
-        global.timestamp.connect = new Date
-      }
-    } catch (e) {
-      conn.logger.error(e)
-    }
-  }, 5000)
-})
-
-global.dfail = (type, m, conn) => {
-  let msg = {
-    rowner: 'Perintah ini hanya dapat digunakan oleh _*Owner Kuhong!*_',
-    owner: 'Perintah ini hanya dapat digunakan oleh _*Owner Bot*_!',
-    mods: '_Perintah ini tidak diaktifkan oleh Owner!_',
-    premium: 'Perintah ini hanya untuk member _*Premium*_ !',
-    group: 'Perintah ini hanya dapat digunakan di grup!',
-    private: 'Perintah ini hanya dapat digunakan di Chat Pribadi!',
-    admin: 'Perintah ini hanya untuk *Admin* grup!',
-    botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini!',
-    unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Manusia.16*'
-  }[type]
-  if (msg) conn.reply(m.chat, msg, m)
-}
 
 if (opts['test']) {
   conn.user = {
@@ -392,11 +87,19 @@ if (opts['test']) {
       }
     }
   }
+
   conn.sendMessage = async (chatId, content, type, opts = {}) => {
     let message = await conn.prepareMessageContent(content, type, opts)
     let waMessage = conn.prepareMessageFromContent(chatId, message, opts)
     if (type == 'conversation') waMessage.key.id = require('crypto').randomBytes(16).toString('hex').toUpperCase()
-    conn.emit('message-new', waMessage)
+    conn.emit('chat-update', {
+      jid: conn.user.jid,
+      messages: {
+        all() {
+          return [waMessage]
+        }
+      }
+    })
   }
   rl.on('line', line => conn.sendMessage('123@s.whatsapp.net', line.trim(), 'conversation'))
 } else {
@@ -405,12 +108,54 @@ if (opts['test']) {
     process.send(line.trim())
   })
   conn.connect().then(() => {
+    fs.writeFileSync(authFile, JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
     global.timestamp.connect = new Date
   })
 }
 process.on('uncaughtException', console.error)
 // let strQuot = /(["'])(?:(?=(\\?))\2.)*?\1/
 
+let isInit = true
+global.reloadHandler = function () {
+  let handler = require('./handler')
+  if (!isInit) {
+    conn.off('chat-update', conn.handler)
+    conn.off('message-delete', conn.onDelete)
+    conn.off('group-add', conn.onAdd)
+    conn.off('group-leave', conn.onLeave)
+  }
+  conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject'
+  conn.bye = 'Selamat tinggal @user!'
+  conn.handler = handler.handler
+  conn.onAdd = handler.welcome
+  conn.onLeave = handler.leave
+  conn.onDelete = handler.delete
+  conn.on('chat-update', conn.handler)
+  conn.on('message-delete', conn.onDelete)
+  conn.on('group-add', conn.onAdd)
+  conn.on('group-leave', conn.onLeave)
+  if (isInit) {
+    conn.on('error', conn.logger.error)
+    conn.on('close', () => {
+      setTimeout(async () => {
+        try {
+          if (conn.state === 'close') {
+            if (fs.existsSync(authFile)) await conn.loadAuthInfo(authFile)
+            await conn.connect()
+            fs.writeFileSync(authFile, JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
+            global.timestamp.connect = new Date
+          }
+        } catch (e) {
+          conn.logger.error(e)
+        }
+      }, 5000)
+    })
+  }
+  isInit = false
+  return true
+}
+
+// Plugin Loader
 let pluginFolder = path.join(__dirname, 'plugins')
 let pluginFilter = filename => /\.js$/.test(filename)
 global.plugins = {}
@@ -423,7 +168,7 @@ for (let filename of fs.readdirSync(pluginFolder).filter(pluginFilter)) {
   }
 }
 console.log(Object.keys(global.plugins))
-global.reload = (event, filename) => {
+global.reload = (_event, filename) => {
   if (pluginFilter(filename)) {
     let dir = path.join(pluginFolder, filename)
     if (dir in require.cache) {
@@ -445,23 +190,31 @@ global.reload = (event, filename) => {
 }
 Object.freeze(global.reload)
 fs.watch(path.join(__dirname, 'plugins'), global.reload)
-
+global.reloadHandler()
 process.on('exit', () => global.DATABASE.save())
 
 
 
 // Quick Test
-let ffmpeg = spawnSync('ffmpeg')
-let ffmpegWebp = spawnSync('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-'])
-let convert = spawnSync('convert')
-global.support = {
-  ffmpeg: ffmpeg.status,
-  ffmpegWebp: ffmpeg.status && ffmpegWebp.stderr.length == 0 && ffmpegWebp.stdout.length > 0,
-  convert: convert.status
+async function _quickTest() {
+  let spawn = promisify(cp.spawn).bind(cp)
+  let [ffmpeg, ffmpegWebp, convert] = await Promise.all([
+    spawn('ffmpeg', [], {}),
+    spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-'], {}),
+    spawn('convert', [], {})
+  ]).catch(conn.logger.error)
+  global.support = {
+    ffmpeg: ffmpeg.status,
+    ffmpegWebp: ffmpeg.status && ffmpegWebp.stderr.length == 0 && ffmpegWebp.stdout.length > 0,
+    convert: convert.status
+  }
+  Object.freeze(global.support)
+
+  if (!global.support.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
+  if (!global.support.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
+  if (!global.support.convert) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
 }
-Object.freeze(global.support)
 
-if (!global.support.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
-if (!global.support.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--emable-ibwebp while compiling ffmpeg)')
-if (!global.support.convert) conn.logger.warn('CREDIT RC047!')
-
+/*_quickTest()
+  .then(() => conn.logger.info('Quick Test Done'))
+  .catch(console.error)*/
