cfae7d6427c3deddd66088e7030463d5f14a262d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-30 10:15:58 +0700

    Ok

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, participants, isPrems }) => {
 let who
   if (m.isGroup) who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   else who = m.chat
-  if (!who) throw 'Tag orang yang akan dijadikan premium!'
+  if (!text) throw 'Tag orang yang akan dijadikan premium!'
  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
   let user = `${who.split("@s.whatsapp.net")[0]}`
   let up = global.prems.push(user)
