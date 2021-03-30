8fcaf98408c3f6bf75cfa79a0d763392f620d16b
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-03-30 10:20:53 +0700

    Ok :v

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'Sticker Creator'
+global.author = 'By RC047 NJENKK'
 
 global.multiplier = 69 // The higher, The harder levelup
 
