057816bfd5685c7786937ab69747cf84f926968e
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-01 12:23:20 +0700

    Update and rename index.js to RendyGans.js
    :V

---
+++ RendyGans.js
@@ -0,0 +1,58 @@
+console.log('1')
+console.log('2')
+console.log('3')
+console.log('MULAII :V')
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('Rendy Gans', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+CFonts.say(`Instagram :\n@rendycraft047`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  p.on('message', data => {
+    console.log('RENDY GANTENG BANGET!', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+  // console.log(p)
+}
+
+start('main.js')
