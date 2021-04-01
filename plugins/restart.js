8a40a16bd79ca72f03db1e7b1dc3f09a85b5eb1d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-01 12:25:50 +0700

    Perubahan kecil

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,10 +1,11 @@
 let { spawn }  = require('child_process');
 let handler  = async (m, { conn }) => {
-  if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+  if (!process.send) throw 'Akses Ditolak!\nJalankan Bot dengan teks\n\n*$ node RendyGans.js*'
   if (global.conn.user.jid == conn.user.jid) {
     await m.reply('Sedang Mereset Bot...\nMohon tunggu sekitar 1 menit')
     await global.DATABASE.save()
     process.send('reset')
+    await m.reply('Done!')
   } else throw '_eeeeeiiittsssss..._'
 }
 handler.help = ['restart']
