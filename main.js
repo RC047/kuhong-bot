ec197840d82bc4d540520726d368434402751697
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-01 12:38:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 require('./config.js')
-require('./RendyGans.js')
 let { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
 let { generate } = require('qrcode-terminal')
 let syntaxerror = require('syntax-error')
