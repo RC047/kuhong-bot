893f3b9f3a38cab057c5f4b3aa3efb549500e764
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-09 19:16:14 +0700

    Ganti biar gak ngebales (Jadikan Bot sebagai admin)

--- plugins/antivirtex.js
+++ plugins/antivirtex.js
@@ -6,23 +6,11 @@ let handler = async function(m, { conn, text }) {
   let name = m.fromMe ? conn.user : conn.contacts[m.sender]
   let users = m.sender
 
-    if (text > 5000) {
+    if (m.text > 5000) {
   conn.reply(m.chat, '*「 ANTI VIRTEX 」*\n\nTerdeteksi *${username}* telah mengirim link virtex!\n\nMaaf Kamu akan dikick dari grup ini!', m)
      await conn.groupRemove(m.chat, [users])
   }
 }
-handler.tags = ['group']
-handler.command = new RegExp
-handler.customPrefix = /^-?[a-z]+(\.[A-Z]+)?$/
-handler.owner = false
-handler.mods = false
-handler.premium = false
 handler.group = true
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = true
-
-handler.fail = null
 
 module.exports = handler
