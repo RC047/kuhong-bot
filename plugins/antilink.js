ade4d7178e0c3deb303a1726021f11943462a747
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-10 17:58:21 +0700

    Fix antilink

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -9,9 +9,7 @@ handler.before = function (m, { user, isAdmin, isBotAdmin }) {
   let name = this.getName(m.sender)
   let link = linkRegex.exec(m.text)
 
-  if (isAdmin && chat.antiLink && link) return m.reply(`*${name}* Adalah Admin! :D\n\nBot tidak akan mengkick!`)
-  if (!isBotAdmin && chat.antiLink && link) return m.reply(`Untung Bot Bukan Admin!\n\nKalo nggak Bot pasti udah ngekick kamu *${name}*`)
-  if (!isAdmin && isBotAdmin && chat.antiLink && link) {
+  if (chat.antiLink && link) {
  m.reply(`*「 ANTI LINK 」*\n\nTerdeteksi *${name}* telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini!`)
    this.groupRemove(m.chat, [m.sender])
   }
